02009fac8886508f0771db5297f76632197e5666
Sachin Goled <62918940+sachingoled@users.noreply.github.com>
2020-04-16 21:23:10 +0530

    Delete aws.bat

--- aws.bat
+++
@@ -1,1 +0,0 @@
-aws ec2 run-instances --image-id ami-0a647b8642eaade60 --count 1 --instance-type t2.micro --key-name Goledsachin --security-group-ids sg-925c74fd --subnet-id subnet-7eb99f16 --region ap-south-1
